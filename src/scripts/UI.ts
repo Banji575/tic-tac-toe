28df59d7bbaf81c1d944f499aa8b3d1aa92939c1
Sergey <banjamin@yandex.ru>
2022-02-01 17:58:56 +0300

    finished basic func

--- src/scripts/UI.ts
+++ src/scripts/UI.ts
@@ -3,6 +3,8 @@ import MainScene from "./scenes/mainScene"
 export class UI {
   text: Phaser.GameObjects.Text
   button: Phaser.GameObjects.Sprite
+  uiRect:Phaser.GameObjects.Graphics
+
   constructor(public scene: MainScene) {
     this.init()
   }
@@ -16,13 +18,15 @@ export class UI {
     this.button = this.scene.add.sprite(width/2, 0 + this.text.height, 'restart').setOrigin(0).setInteractive().setVisible(false).setDepth(30)
     this.button.on('pointerdown', this.onClick,this)
 
-
+    this.uiRect = this.scene.add.graphics().setDepth(29).setVisible(false);
+    this.uiRect.fillStyle(0x042A3D,1);
+    this.uiRect.fillRoundedRect(-10, -10, 2000, 2000)
   }
 
   private onClick(){
       this.scene.restart()
       this.button.setVisible(false)
-
+      this.uiRect.setVisible(false)
   }
   public renderText(text: string) {
     this.text.text = text
@@ -30,6 +34,6 @@ export class UI {
 
   public showRestartButton(){
       this.button.setVisible(true)
-    
+      this.uiRect.setVisible(true).setAlpha(0.6)
   }
 }
